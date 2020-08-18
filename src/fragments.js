bb83ba8d3d9de710df0cdf9ee59dcde846d8c386
zereight <zereight@zereightui-MacBookPro.local>
2020-08-18 23:45:20 +0900

    User, Post refactoring

--- src/fragments.js
+++ src/fragments.js
@@ -1,14 +1,34 @@
 export const USER_FRAGMENT = `
-    fragment UserParts on User{
-        id 
-        username
-        email
-        firstName
-        lastName
-        following { 
-            
-            caption
+    id
+    username
+`;
+
+export const COMMENT_FRAGMENT = `
+    id
+    text
+    user {
+        ${USER_FRAGMENT}
+    }
+`;
+
+export const FILE_FRAGMENT = `
+    id
+    url
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post{
+        id
+        location
+        caption
+        files {
+            ${FILE_FRAGMENT}
+        }
+        comments {
+            ${COMMENT_FRAGMENT}
+        }
+        user {
+            ${USER_FRAGMENT}
         }
     }
-    
-  `;
+`;
